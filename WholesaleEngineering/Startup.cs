6dc17775e8867fd60507948f040ab3e787d4983f
Rajini <crajini.cse@gmail.com>
2021-01-26 13:27:50 +0530

    initial

--- WholesaleEngineering/Startup.cs
+++ WholesaleEngineering/Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging; 
 using Microsoft.OpenApi.Models;
 using Models;
 using Repository;
